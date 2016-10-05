f2cf22182d8bc7e18502b4412a3770d65e1c9103
tankwan <tankwantiong@gmail.com>
2016-10-05 16:55:36 -0700

    passed post test

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,6 +5,7 @@ var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  var checkURL = url.parse(req.url);
   if (req.method === 'GET') {
     if (req.url === '/') {
       res.statusCode = 200;
@@ -18,27 +19,49 @@ exports.handleRequest = function (req, res) {
         res.end();
       });
     } else {
-      var checkURL = url.parse(req.url);
       var path = 'test/testdata/sites' + checkURL.pathname;
-      console.log(path);
       fs.exists(path, function(exists) {
         if (!exists) {
-          console.log('not exists');
           res.statusCode = 404;
           res.end();
+        } else {
+          fs.readFile(path, 'utf-8', function(error, data) {
+            if (error) {
+              throw error;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html'); //?
+            res.write(data);
+            res.end();
+          });
         }
-        fs.readFile(path, 'utf-8', function(error, data) {
-          if (error) {
-            throw error;
-          }
-          console.log('pathdata', data);
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/html'); //?
-          res.write(data);
-          res.end();
-        });
       });
     }
+  }
+  if (req.method === 'POST') {
+    var chum = '';
+    req.on('data', function(chunk) {
+      chum += chunk;
+      chum = chum.slice(4);
+
+    });
+
+    fs.readFile('test/testdata/sites.txt','utf-8' ,function(err, data){
+      if (err) {
+        throw err;
+      } else {
+        
+          data = data + chum + '\n';
+          fs.writeFile('test/testdata/sites.txt',data, function(err) {
+            if (err) {
+              throw err;
+            }
+            res.statusCode = 302;
+            console.log('you did it!', data);
+            res.end();
+          })   
+      }
+    })
   }
   // res.end(archive.paths.list);
 };
