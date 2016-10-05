2f9268c2ea83122f4c49d75c17a8e2adea74eeff
tankwan <tankwantiong@gmail.com>
2016-10-05 15:49:05 -0700

    Write code to pass Get /pathname request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,20 +1,44 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
+var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
   if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html'); //?
-    // res.setHeader()
-    fs.readFile('web/public/index.html', 'utf-8', function(error, data) {
-      if (error) {
-        throw error;
-      }
-      res.write(data);
-      res.end();
-    });
+    if (req.url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html'); //?
+      // res.setHeader()
+      fs.readFile('web/public/index.html', 'utf-8', function(error, data) {
+        if (error) {
+          throw error;
+        }
+        res.write(data);
+        res.end();
+      });
+    } else {
+      var checkURL = url.parse(req.url);
+      var path = 'test/testdata/sites' + checkURL.pathname;
+      console.log(path);
+      fs.exists(path, function(exists) {
+        if (!exists) {
+          console.log('not exists');
+          res.statusCode = 404;
+          res.end();
+        }
+        fs.readFile(path, 'utf-8', function(error, data) {
+          if (error) {
+            throw error;
+          }
+          console.log('pathdata', data);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html'); //?
+          res.write(data);
+          res.end();
+        });
+      });
+    }
   }
   // res.end(archive.paths.list);
 };
