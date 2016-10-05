50095b6eb29a5a56de760efa9c83f2596d32b75f
tankwan <tankwantiong@gmail.com>
2016-10-05 14:51:41 -0700

    Write code to pass test for 'GET /'

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html'); //?
+    // res.setHeader()
+    fs.readFile('web/public/index.html', 'utf-8', function(error, data) {
+      if (error) {
+        throw error;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+  // res.end(archive.paths.list);
 };
