9b9f4a8578d43efbdb4e8188afc6d1171911d091
tankwan <tankwantiong@gmail.com>
2016-10-05 17:30:15 -0700

    Write helper functions

--- web/request-handler.js
+++ web/request-handler.js
@@ -43,25 +43,22 @@ exports.handleRequest = function (req, res) {
     req.on('data', function(chunk) {
       chum += chunk;
       chum = chum.slice(4);
-
     });
 
-    fs.readFile('test/testdata/sites.txt','utf-8' ,function(err, data){
+    fs.readFile('test/testdata/sites.txt', 'utf-8', function(err, data){
       if (err) {
         throw err;
       } else {
-        
-          data = data + chum + '\n';
-          fs.writeFile('test/testdata/sites.txt',data, function(err) {
-            if (err) {
-              throw err;
-            }
-            res.statusCode = 302;
-            console.log('you did it!', data);
-            res.end();
-          })   
+        data = data + chum + '\n';
+        fs.writeFile('test/testdata/sites.txt',data, function(err) {
+          if (err) {
+            throw err;
+          }
+          res.statusCode = 302;
+          console.log('you did it!', data);
+          res.end();
+        });   
       }
-    })
+    });
   }
-  // res.end(archive.paths.list);
 };
